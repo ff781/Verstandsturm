4c0e5005026a64493870aaae2f8cbad2275ddca2
NKayser <nico.a.kayser@gmail.com>
2021-11-22 12:58:45 +0100

    Use 3d color values

--- src/bot/nav/SLine.java
+++ src/bot/nav/SLine.java
@@ -17,10 +17,10 @@ public class SLine {
   public void exec(Bot bot){
 	  LCD.clear();			
 	  System.out.println("Running simple line subroutine");
-	  float w = 0.5f;								// target brightness
-	  float K_p = 0.3f;								// constant for P-Regler
+	  float[] w = {0.163f, 0.283f, 0.133f};			// target rgb values for white
+	  float K_p = 30;								// constant for P-Regler
 	  
-	  float distance = 300;							// in cm
+	  float distance = 150;							// in cm
 	  float speed = 2;								// cm per second
 	  int direction = 1;							// 1: forward
 	  boolean blocking = false;						// don't block thread
@@ -28,34 +28,43 @@ public class SLine {
 	  bot.driver.drive(distance, speed, direction, blocking);
 	  
 	  while (true) {
-		  if (followLineTask(bot, w, K_p)) return;
-		  if (motorsStopped(bot)) return;
-		  if (Button.ESCAPE.isDown()) return;
+		  if (followLineTask(bot, w, K_p) || motorsStopped(bot) || Button.ESCAPE.isDown()) {
+			  LCD.clear();
+			  return;
+		  }
 	  }
-  }
-  
-  private float getBrightness(float[] rgb) {
-	  return (rgb[0] + rgb[1] + rgb[2]) / 3;
   }
   
   private boolean isBlue(float[] rgb) {
 	  return rgb[0] < 0.04f && rgb[1] < 0.11f && rgb[2] > 0.06f ;
   }
   
+  private float getBrightness(float[] rgb) {
+	  return (rgb[0] + rgb[1] + rgb[2]) / 3;
+  }
+  
+  private float getMeanError(float[] r, float[] w) {
+	  assert r.length == w.length;
+	  float accumulator = 0;
+	  for (int i = 0; i < r.length; i++) {
+		  accumulator += (r[i] - w[i]);
+	  }
+	  return accumulator / r.length;
+  }
+  
   private boolean motorsStopped(Bot bot) {
 	  return !bot.lMotor.isMoving() && !bot.rMotor.isMoving();
   }
   
-  private boolean followLineTask(Bot bot, float w, float K_p) {
-	  float[] rgb = bot.sensors.getRGB();
+  private boolean followLineTask(Bot bot, float[] w, float K_p) {
+	  float[] r = bot.sensors.getRGB();
 	  
-	  if (isBlue(rgb)) return true;
-	  
-	  float r = getBrightness(rgb);					// measured brightness
-	  float x_d = r - w;						// error
+	  if (isBlue(r)) return true;
+	  				// measured brightness
+	  float x_d = getMeanError(r, w);						// error
 	  float y = x_d * K_p;						// adjustment
 	  //System.out.println("rgb: " + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ". "
-	  System.out.println("x_d: " + String.format("%.3f", x_d) + ", y: " + String.format("%.3f", y));
+	  System.out.println("d " + String.format("%.3f", x_d) + " y " + String.format("%.3f", y));
 	  bot.driver.directionChange(y);
 	  
 	  return false;
