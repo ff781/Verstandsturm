2be673350746aef82c715592484e28e8e3a5deed
NKayser <nico.a.kayser@gmail.com>
2021-11-22 11:25:26 +0100

    Give initial speed, check blue

--- src/bot/nav/SLine.java
+++ src/bot/nav/SLine.java
@@ -20,18 +20,39 @@ public class SLine {
 	  float w = 0.5f;								// target brightness
 	  float K_p = 0.3f;								// constant for P-Regler
 	  
-	  while (Button.ESCAPE.isUp()) {
-		  float[] rgb = bot.sensors.getRGB();
-		  float r = getBrightness(rgb);					// measured brightness
-		  float x_d = r - w;						// error
-		  float y = x_d * K_p;						// adjustment
-		  System.out.println("rgb: " + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ". x_d: " + x_d + ", y: " + y);
-		  bot.driver.directionChange(y);
+	  float distance = 100;							// in cm
+	  float speed = 2;								// cm per second
+	  int direction = 1;							// 1: forward
+	  
+	  bot.driver.drive(distance, speed, direction);
+	  
+	  while (true) {
+		  if (followLineTask(bot, w, K_p)) return;
+		  //if (Button.ESCAPE.isDown()) return;
 	  }
   }
   
   private float getBrightness(float[] rgb) {
 	  return (rgb[0] + rgb[1] + rgb[2]) / 3;
+  }
+  
+  private boolean isBlue(float[] rgb) {
+	  return rgb[0] < 0.04f && rgb[1] < 0.11f && rgb[2] > 0.06f ;
+  }
+  
+  private boolean followLineTask(Bot bot, float w, float K_p) {
+	  float[] rgb = bot.sensors.getRGB();
+	  
+	  if (isBlue(rgb)) return true;
+	  
+	  float r = getBrightness(rgb);					// measured brightness
+	  float x_d = r - w;						// error
+	  float y = x_d * K_p;						// adjustment
+	  //System.out.println("rgb: " + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ". "
+	  System.out.println("x_d: " + x_d + ", y: " + y);
+	  bot.driver.directionChange(y);
+	  
+	  return false;
   }
 
 }
