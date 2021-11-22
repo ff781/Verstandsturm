8a1d21b213146d9895fc95c16f6e30c95cb7e560
NKayser <nico.a.kayser@gmail.com>
2021-11-22 11:55:09 +0100

    Stop when motors stop

--- src/bot/nav/SLine.java
+++ src/bot/nav/SLine.java
@@ -20,15 +20,17 @@ public class SLine {
 	  float w = 0.5f;								// target brightness
 	  float K_p = 0.3f;								// constant for P-Regler
 	  
-	  float distance = 100;							// in cm
+	  float distance = 300;							// in cm
 	  float speed = 2;								// cm per second
 	  int direction = 1;							// 1: forward
+	  boolean blocking = false;						// don't block thread
 	  
-	  bot.driver.drive(distance, speed, direction);
+	  bot.driver.drive(distance, speed, direction, blocking);
 	  
 	  while (true) {
 		  if (followLineTask(bot, w, K_p)) return;
-		  //if (Button.ESCAPE.isDown()) return;
+		  if (motorsStopped(bot)) return;
+		  if (Button.ESCAPE.isDown()) return;
 	  }
   }
   
@@ -40,6 +42,10 @@ public class SLine {
 	  return rgb[0] < 0.04f && rgb[1] < 0.11f && rgb[2] > 0.06f ;
   }
   
+  private boolean motorsStopped(Bot bot) {
+	  return !bot.lMotor.isMoving() && !bot.rMotor.isMoving();
+  }
+  
   private boolean followLineTask(Bot bot, float w, float K_p) {
 	  float[] rgb = bot.sensors.getRGB();
 	  
@@ -49,7 +55,7 @@ public class SLine {
 	  float x_d = r - w;						// error
 	  float y = x_d * K_p;						// adjustment
 	  //System.out.println("rgb: " + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ". "
-	  System.out.println("x_d: " + x_d + ", y: " + y);
+	  System.out.println("x_d: " + String.format("%.3f", x_d) + ", y: " + String.format("%.3f", y));
 	  bot.driver.directionChange(y);
 	  
 	  return false;
